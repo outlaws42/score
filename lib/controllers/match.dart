aa14e3aa35e529ea9b164345b22f4a9c9eb04660
outlaws42 <outlaws42@gmail.com>
2021-09-13 19:18:21 -0400

    fix names of variables

--- lib/controllers/match.dart
+++ lib/controllers/match.dart
@@ -16,16 +16,16 @@ class MatchProvider extends ChangeNotifier {
         .map(
           (match) => MatchModel(
             id: match['id'] as int?,
-            matchName: match['matchname'] as String?,
+            matchName: match['match_name'] as String?,
             player1Name: match['player1_name'] as String?,
             player2Name: match['player1_name'] as String?,
-            playerId1: match['player1_id'] as int?,
-            playerId2: match['player2_id'] as int?,
+            player1Id: match['player1_id'] as int?,
+            player2Id: match['player2_id'] as int?,
             // playerId3: match['player3_id'] as int?,
             // playerId4: match['player4_id'] as int?,
             // playerId5: match['player5_id'] as int?,
-            playerScore1: match['player_score1'] as int?,
-            playerScore2: match['player_score2'] as int?,
+            player1Score: match['player1_score'] as int?,
+            player2Score: match['player2_score'] as int?,
             // playerScore3: match['playerscore3'] as int?,
             // playerScore4: match['playerscore4'] as int?,
             // playerScore5: match['playerscore5'] as int?,
@@ -42,33 +42,40 @@ class MatchProvider extends ChangeNotifier {
 
   Future<void> addMatch({
     // int id,
-    String matchname = "",
+    String matchName = "",
     String? player1Name,
     String? player2Name,
     int? player1Id,
     int? player2Id,
-    int? endscore,
-    int playerscore1 = 0,
-    int playerscore2 = 0,
+    int? endScore,
+    int player1Score = 0,
+    int player2Score = 0,
     String? gameName,
-    int? gameid,
-    bool iscompleted = false,
+    int? gameId,
+    bool isCompleted = false,
   }) async {
     final newMatch = MatchModel(
       // id: id,
-      matchName: matchname,
-      playerScore1: playerscore1,
-      playerScore2: playerscore2,
-      isComplete: iscompleted,
+      matchName: matchName,
+      player1Name: player1Name,
+      player2Name: player2Name,
+      player1Id: player1Id,
+      player2Id: player2Id,
+      player1Score: player1Score,
+      player2Score: player2Score,
+      winScore: endScore,
+      gameName: gameName,
+      gameId: gameId,
+      isComplete: isCompleted,
     );
     _matches.add(newMatch);
     notifyListeners();
     DBHelper.insert('indv_matches', {
       // 'id': newPlayer.id,
-      'matchname': newMatch.matchName,
-      'iscomplete': newMatch.isComplete,
-      'playerscore1': newMatch.playerScore1,
-      'playerscore2': newMatch.playerScore2,
+      'match_name': newMatch.matchName,
+      'is_complete': newMatch.isComplete,
+      'player1_score': newMatch.player1Score,
+      'player2_score': newMatch.player2Score,
     });
   }
 
--- lib/models/match_model.dart
+++ lib/models/match_model.dart
@@ -7,13 +7,13 @@ class MatchModel with ChangeNotifier {
   final String? matchName;
   final String? player1Name;
   final String? player2Name;
-  final int? playerId1;
-  final int? playerId2;
+  final int? player1Id;
+  final int? player2Id;
   // final int? playerId3;
   // final int? playerId4;
   // final int? playerId5;
-  final int? playerScore1;
-  final int? playerScore2;
+  final int? player1Score;
+  final int? player2Score;
   // final int? playerScore3;
   // final int? playerScore4;
   // final int? playerScore5;
@@ -28,13 +28,13 @@ class MatchModel with ChangeNotifier {
     this.matchName,
     this.player1Name,
     this.player2Name,
-    this.playerId1,
-    this.playerId2,
+    this.player1Id,
+    this.player2Id,
     // this.playerId3 = 0,
     // this.playerId4 = 0,
     // this.playerId5 = 0,
-    this.playerScore1,
-    this.playerScore2,
+    this.player1Score,
+    this.player2Score,
     // this.playerScore3 = 0,
     // this.playerScore4 = 0,
     // this.playerScore5 = 0,
